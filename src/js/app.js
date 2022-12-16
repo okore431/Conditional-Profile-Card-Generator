f17137fbbd84799163b7deb95f2bc05405eac992
okore431 <olussskin@gmail.com>
2022-12-16 01:37:06 +0000

    Fixed LinkedIn

--- src/js/app.js
+++ src/js/app.js
@@ -54,7 +54,7 @@ function render(variables = {}) {
           <ul class="${variables.socialMediaPosition}"> 
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin} "><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/${variables.linkedin} "><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
